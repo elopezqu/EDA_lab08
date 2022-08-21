12d93f58a239e24a55891d7b518830ff22166c7e
Diego Pacori <dpacoria@unsa.edu.pe>
2022-08-21 18:51:45 -0500

    Se agrego todo a una carpeta grafo

---
+++ problema5/Grafo/Grafo.java
@@ -0,0 +1,44 @@
+
+
+import java.util.ArrayList;
+
+public class Grafo {
+    private ArrayList<Node> cabezas;
+
+    public Grafo() {
+        cabezas = new ArrayList<Node>();
+    }
+
+    public int buscarIndex(String nombre) {
+        for (int i = 0; i < cabezas.size(); i++) {
+            if (cabezas.get(i).getNombre().equals(nombre)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public void newVertice(String nombre) {
+        if (buscarIndex(nombre) == -1) {
+            cabezas.add(new Node(nombre));
+        }
+    }
+
+    public void conectar(String inicio, String llegada) {
+        if (!cabezas.get(buscarIndex(inicio)).getAdya().contains(buscarIndex(llegada))) {
+            cabezas.get(buscarIndex(inicio)).setAdya(buscarIndex(llegada));;
+        }
+    }
+    public String toString(){
+        String aux = "";
+        for(Node node : cabezas){
+            aux += "Vertice: "+node.getNombre();
+            for(int index : node.getAdya()){
+                aux += " ---> "+ cabezas.get(index).getNombre();
+            }
+            aux += "\n";
+        }
+        return aux;
+    }
+
+}
---
+++ problema5/Grafo/Node.java
@@ -0,0 +1,22 @@
+
+
+import java.util.ArrayList;
+
+public class Node {
+    private String nombre;
+    private ArrayList<Integer> adya;
+
+    public Node(String nombre){
+        this.nombre = nombre;
+        this.adya = new ArrayList<Integer>();
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setAdya(int i){
+        this.adya.add(i);
+    }
+    public ArrayList<Integer> getAdya(){
+        return this.adya;
+    }
+}
